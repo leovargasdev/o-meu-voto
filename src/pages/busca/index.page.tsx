353c0312507c1c819ccd22e7da1f14cf24255b80
Leonardo Vargas <leu1607@hotmail.com>
2024-08-29 20:53:14 -0300

    feat: remove path "cidade" and components in /busca

--- src/pages/busca/index.page.tsx
+++ src/pages/busca/index.page.tsx
@@ -1,11 +1,103 @@
-import { AnimationForm } from './components/AnimationForm'
+import { useState } from 'react'
+import Select from 'react-select'
+import classNames from 'classnames'
+import { useRouter } from 'next/router'
+
+import type { Option } from 'types'
+import { MapBrazil } from 'components'
+import { maskToParamsURL } from 'utils/mask'
+import { citiesByState } from 'data/cities/'
 
 import styles from './styles.module.scss'
 
-const SearchPage = () => (
-  <div className={styles.container}>
-    <AnimationForm />
+interface QuestionProps {
+  label: string
+  enabled: boolean
+  children: React.ReactNode
+}
+
+const Question = ({ label, enabled, children }: QuestionProps) => (
+  <div className={classNames(styles.carrousel__item, enabled && styles.enable)}>
+    <div className={styles.question}>
+      <label className={styles.label}>{label}</label>
+      {children}
+    </div>
   </div>
 )
+
+const SearchPage = () => {
+  const router = useRouter()
+  const [question, setQuestion] = useState<number>(0)
+  const [state, setState] = useState<string>('')
+  const [city, setCity] = useState<Option | null>()
+
+  const handleQuestionUF = (value: string) => {
+    setState(value)
+    setQuestion(1)
+  }
+
+  const handleQuestionCity = (option: Option | null) => {
+    setCity(option)
+    setQuestion(2)
+  }
+
+  const handleSearch = (role?: string) => {
+    if (!city) return
+
+    const roleQuery = role ? `?role=${role}` : ''
+    const cityPath = city.value + '-' + maskToParamsURL(city.label)
+    const route = `/${state}/${cityPath}/candidatos` + roleQuery
+
+    router.push(route)
+  }
+
+  const cities = state ? citiesByState[state] : []
+  return (
+    <div className={styles.container}>
+      <button>
+        <img src="/logo.png" width={150} height="auto" />
+      </button>
+
+      <div
+        className={styles.content}
+        style={{ transform: `translateX(${-100 * question}vw)` }}
+      >
+        <Question label="Escolha um estado do Brasil" enabled={question === 0}>
+          <div className={styles.map}>
+            <MapBrazil selectState={handleQuestionUF} />
+          </div>
+        </Question>
+
+        <Question label="Selecione um munípio" enabled={question === 1}>
+          <div className={styles.cities}>
+            <Select
+              placeholder="Selecionar"
+              isSearchable
+              options={cities}
+              onChange={handleQuestionCity}
+            />
+          </div>
+        </Question>
+
+        <Question
+          label="Escolha o cargo do pré-candidato"
+          enabled={question === 2}
+        >
+          <div className={styles.roles}>
+            <button type="button" onClick={() => handleSearch('11')}>
+              Prefeito
+            </button>
+            <button type="button" onClick={() => handleSearch('12')}>
+              Vice-prefeito
+            </button>
+            <button type="button" onClick={() => handleSearch('13')}>
+              Vereador
+            </button>
+          </div>
+        </Question>
+      </div>
+    </div>
+  )
+}
 
 export default SearchPage
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import { NextPage } from 'next'
 
 import styles from '../styles/home.module.scss'
@@ -6,6 +7,10 @@ import styles from '../styles/home.module.scss'
 const LoginPage: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Meu voto 2024 | Veja a lista dos pré-candidatos</title>
+      </Head>
+
       <img src="/logo.png" />
       <h1>Veja a lista dos pré-candidatos do seu munípicio</h1>
 
