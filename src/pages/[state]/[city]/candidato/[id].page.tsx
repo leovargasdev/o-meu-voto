8e675a94b496d615823c2967a71458cde34ab626
Leonardo Vargas <leu1607@hotmail.com>
2024-08-31 19:08:45 -0300

    feat: add breadcrumb

--- src/pages/[state]/[city]/candidato/[id].page.tsx
+++ src/pages/[state]/[city]/candidato/[id].page.tsx
@@ -1,7 +1,12 @@
 import classNames from 'classnames'
 import type { Candidate } from 'types/candidate'
 import { serviceGetCandidate } from 'services'
-import { Mailbox } from '@phosphor-icons/react'
+import {
+  CaretRight,
+  City,
+  IdentificationCard,
+  Mailbox
+} from '@phosphor-icons/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 
 import { Layout } from 'components'
@@ -11,34 +16,61 @@ import { News } from './components/News'
 import { Properties } from './components/Properties'
 
 import styles from './styles.module.scss'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const CandidatePage = (candidate: Candidate) => {
+  const router = useRouter()
+
   if (!candidate) {
     return <></>
   }
 
+  const hostCity = router.asPath.split('/candidato/')[0]
+
   return (
     <Layout>
       <div className={styles.container}>
-        <Profile {...candidate} />
+        <div className={styles.breadcrumb}>
+          <Link href="/">Eleições 2024</Link>
 
-        <div className={classNames('card', styles.content)}>
-          <Tags {...candidate} />
+          <CaretRight size={12} />
 
-          {candidate.emails && (
-            <div>
-              <h2>
-                <Mailbox />
-                Emails para contato
-              </h2>
+          {/* router.goback() ??? */}
+          <Link href={hostCity + '/candidatos'}>
+            <City size={17} />
+            Candidatos de {candidate.localCandidatura}
+          </Link>
 
-              <p>{candidate.emails.join(', ')}</p>
-            </div>
-          )}
+          <CaretRight size={12} />
 
-          <News {...candidate} />
+          <span>
+            <IdentificationCard size={17} />
+            Candidato(a) {candidate.nomeUrna.toLocaleLowerCase()}
+          </span>
+        </div>
 
-          <Properties {...candidate} />
+        <div className={styles.content}>
+          <Profile {...candidate} />
+
+          <div className={classNames('card', styles.info)}>
+            <Tags {...candidate} />
+
+            {candidate.emails && (
+              <div>
+                <h2>
+                  <Mailbox />
+                  Emails para contato
+                </h2>
+
+                <p>{candidate.emails.join(', ')}</p>
+              </div>
+            )}
+
+            <News {...candidate} />
+
+            <Properties {...candidate} />
+          </div>
         </div>
       </div>
     </Layout>
